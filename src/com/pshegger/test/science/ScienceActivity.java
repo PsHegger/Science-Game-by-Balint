199acb6ab7ace87014d91f20d086c196b0b086b9
PsHegger <pshegger@gmail.com>
2011-08-18 10:15:06 +0200

    added guess check

--- src/com/pshegger/test/science/ScienceActivity.java
+++ src/com/pshegger/test/science/ScienceActivity.java
@@ -1,7 +1,9 @@
 package com.pshegger.test.science;
 
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -13,7 +15,7 @@ public class ScienceActivity extends Activity {
 	public static final int MENU_HISTORY = MENU_NEW_RULES + 1;
 	
 	private Rule[] rules;
-	private RadioGroup szin, szam, forma, kitoltes;
+	private RadioGroup szinGrp, szamGrp, formaGrp, kitoltesGrp;
 	
     /** Called when the activity is first created. */
     @Override
@@ -30,10 +32,10 @@ public class ScienceActivity extends Activity {
 			}
 		});
         
-        this.szin = (RadioGroup) findViewById(R.id.szin);
-        this.szam = (RadioGroup) findViewById(R.id.szam);
-        this.forma = (RadioGroup) findViewById(R.id.forma);
-        this.kitoltes = (RadioGroup) findViewById(R.id.kitoltes);
+        this.szinGrp = (RadioGroup) findViewById(R.id.szin);
+        this.szamGrp = (RadioGroup) findViewById(R.id.szam);
+        this.formaGrp = (RadioGroup) findViewById(R.id.forma);
+        this.kitoltesGrp = (RadioGroup) findViewById(R.id.kitoltes);
         
         reset();
     }
@@ -41,7 +43,7 @@ public class ScienceActivity extends Activity {
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		menu.add(0, MENU_NEW_RULES, 0, "Új szabályok");
-		menu.add(0, MENU_HISTORY, 0, "Előzmények");
+		// menu.add(0, MENU_HISTORY, 0, "Előzmények");
 		
 		return super.onCreateOptionsMenu(menu);
 	}
@@ -64,29 +66,106 @@ public class ScienceActivity extends Activity {
 	}
 
 	private void reset() {
-		rules = new Rule[4];
+		this.rules = new Rule[4];
 		
-		rules[0] = new Rule(true);
+		this.rules[0] = new Rule(true);
 		
 		boolean conflict = false;
 		do {
-			rules[1] = new Rule(false);
-			conflict = rules[1].conflict(rules[0]);
+			this.rules[1] = new Rule(false);
+			conflict = this.rules[1].conflict(this.rules[0]);
 		} while(conflict);
 		do {
-			rules[2] = new Rule(false);
-			if (rules[2].conflict(rules[0])) break;
-			conflict = rules[2].conflict(rules[1]);
+			this.rules[2] = new Rule(false);
+			if (this.rules[2].conflict(this.rules[0])) break;
+			conflict = this.rules[2].conflict(this.rules[1]);
 		} while(conflict);
 		do {
-			rules[3] = new Rule(false);
-			if (rules[3].conflict(rules[0])) break;
-			if (rules[3].conflict(rules[1])) break;
-			conflict = rules[3].conflict(rules[2]);
+			this.rules[3] = new Rule(false);
+			if (this.rules[3].conflict(this.rules[0])) break;
+			if (this.rules[3].conflict(this.rules[1])) break;
+			conflict = this.rules[3].conflict(this.rules[2]);
 		} while(conflict);
+		
+		Log.d("Science", this.rules[0].toString());
+		Log.d("Science", this.rules[1].toString());
+		Log.d("Science", this.rules[2].toString());
+		Log.d("Science", this.rules[3].toString());
 	}
 
 	private void check() {
+		boolean bang = false;
+		int szin = Constants.RULE_BARMI;
+		int szam = Constants.RULE_BARMI;
+		int forma = Constants.RULE_BARMI;
+		int kitoltes = Constants.RULE_BARMI;
 		
+		switch (this.szinGrp.getCheckedRadioButtonId()) {
+		case R.id.piros:
+			szin = Constants.SZIN_PIROS;
+			break;
+		case R.id.kek:
+			szin = Constants.SZIN_KEK;
+			break;
+		case R.id.zold:
+			szin = Constants.SZIN_ZOLD;
+			break;
+		}
+		
+		switch (this.szamGrp.getCheckedRadioButtonId()) {
+		case R.id.egy:
+			szam = Constants.SZAM_EGY;
+			break;
+		case R.id.ketto:
+			szam = Constants.SZAM_KETTO;
+			break;
+		case R.id.harom:
+			szam = Constants.SZAM_HAROM;
+			break;
+		}
+		
+		switch (this.formaGrp.getCheckedRadioButtonId()) {
+		case R.id.haromszog:
+			forma = Constants.FORMA_HAROMSZOG;
+			break;
+		case R.id.negyzet:
+			forma = Constants.FORMA_NEGYZET;
+			break;
+		case R.id.kor:
+			forma = Constants.FORMA_KOR;
+			break;
+		}
+		
+		switch (this.kitoltesGrp.getCheckedRadioButtonId()) {
+		case R.id.csikos:
+			kitoltes = Constants.KITOLTES_CSIKOS;
+			break;
+		case R.id.pottyos:
+			kitoltes = Constants.KITOLTES_POTTYOS;
+			break;
+		case R.id.ures:
+			kitoltes = Constants.KITOLTES_URES;
+			break;
+		}
+		
+		Rule guess = new Rule(szin, szam, forma, kitoltes);
+		
+		for (int i = 0; i < this.rules.length; i++) {
+			if (rules[i].check(guess))
+				bang = true;
+		}
+		
+		if (bang) {
+			AlertDialog.Builder builder = new AlertDialog.Builder(this);
+			builder.setMessage("Felrobbant");
+			AlertDialog alert = builder.create();
+			alert.show();
+		}else{
+			AlertDialog.Builder builder = new AlertDialog.Builder(this);
+			builder.setMessage("Stabill");
+			AlertDialog alert = builder.create();
+			alert.show();
+		}
+			
 	}
 }
